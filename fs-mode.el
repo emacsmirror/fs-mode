846a7c7bba6f338c282ba79e9ee16b06f97d20c0
zxsu <zxsu@zxsu.sz>
2013-05-10 17:30:29 +0800

    add upside warning threshold.

--- fs-mode.el
+++ fs-mode.el
@@ -32,6 +32,39 @@
   :version "0.1"
   :group 'applications)
 
+(defcustom fs-mode-warning-threshold 90
+  "Up threshold to warning user, when the disk is full."
+  :type 'integer
+  :group 'fs-mode)
+
+(defface fs-mode-face-1
+  '((t (:inherit font-lock-type-face)))
+  "First fs face")
+
+(defface fs-mode-face-2 
+  '((t (:inherit font-lock-constant-face)))
+  "Second fs face")
+
+(defface fs-mode-face-3
+  '((t (:inherit font-lock-variable-name-face)))
+  "fs face")
+
+(defface fs-mode-face-warning
+  '((t (:inherit font-lock-warning-face)))
+  "fs warning face")
+
+(defface fs-mode-face-4 
+  '((t (:inherit font-lock-comment-delimiter-face)))
+  "fs face")
+
+(defface fs-mode-face-5
+  '((t (:inherit font-lock-function-name-face)))
+  "fs face")
+
+(defface fs-mode-face-6
+  '((t (:inherit font-lock-keyword-face)))
+  "fs face")
+
 (defvar fs-mode-buffer-name "*fs-mode*"
   "Default fs-mode buffer name.")
 
@@ -56,9 +89,22 @@
   ""
   )
 
+(defun fs-warning-p (percent)
+  "Return t if percent is greater than `fs-mode-warning-threshold'."
+  (when (string-match "\\([0-9]+\\)" percent)
+    (let ((int (string-to-int (match-string 1 percent)))
+          (threshold (if (integerp fs-mode-warning-threshold)
+                         fs-mode-warning-threshold
+                       (string-to-int fs-mode-warning-threshold))))
+      (if (> int threshold)
+          t
+        nil)))
+  )
+
 (defun fs-mode-refresh ()
   "Refresh fs-mode buffer."
   (setq tabulated-list-entries nil)
+  (setq fs-mode-all-list nil)
   (let ((output (shell-command-to-string "df -h")))
     ;;(setq fs-mode-parser (start-fs-mode-parser output))
     (dolist (line (split-string output "\n" t))
@@ -66,21 +112,23 @@
              t)
             (t 
              (let ((res (split-string line " " t)))
-               (message "%s" (length res))
                (when (= 6 (length res))
-                 (message "Filesystem: %s" (nth 0 res))
-                 (message "Size: %s" (nth 1 res))
-                 (message "Used: %s" (nth 2 res))
-                 (message "Avail: %s" (nth 3 res))
-                 (message "Use: %s" (nth 4 res))
-                 (message "Mount on: %s" (nth 5 res))
-                 (add-to-list 'fs-mode-all-list 
-                              (list :filesystem (nth 0 res)
-                                    :size (nth 1 res)
-                                    :used (nth 2 res)
-                                    :avail (nth 3 res)
-                                    :use% (nth 4 res)
-                                    :mounton (nth 5 res)))
+                 (let ((filesystem (nth 0 res))
+                       (size (nth 1 res))
+                       (used (nth 2 res))
+                       (avail (nth 3 res))
+                       (use (nth 4 res))
+                       (mounton (nth 5 res)))
+                   (add-to-list 'fs-mode-all-list 
+                              (list :filesystem (propertize filesystem 'face 'fs-mode-face-1)
+                                    :size (propertize size 'face 'fs-mode-face-2)
+                                    :used (propertize used 'face 'fs-mode-face-3)
+                                    :avail (propertize avail 'face 'fs-mode-face-4)
+                                    :use% (propertize use 'face (if (fs-warning-p use)
+                                                                            'fs-mode-face-warning
+                                                                          'fs-mode-face-5))
+                                    :mounton (propertize mounton 'face 'fs-mode-face-6))))
+                 
                  )))
           ))
     (dolist (p fs-mode-all-list)
@@ -97,12 +145,12 @@
 ;;;###autoload
 (define-derived-mode fs-mode tabulated-list-mode "File System mode"
   "Major mode for Linux File System management."
-  (setq tabulated-list-format [("Filesystem" 10 t)
-                               ("Size" 5 t)
-                               ("Used" 5 t)
+  (setq tabulated-list-format [("Filesystem" 15 t)
+                               ("Size" 10 t)
+                               ("Used" 10 t)
                                ("Avail" 5 t)
-                               ("Use%" 5 t)
-                               ("Mounted on" 10 t)])
+                               ("Use%%" 10 t)
+                               ("Mounted on" 20 t)])
   (setq tabulated-list-sort-key nil)
   (add-hook 'tabulated-list-revert-hook 'fs-mode-refresh nil t)
   (tabulated-list-init-header))
